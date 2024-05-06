ceb4745bb678adf5e05ce9bb8b8d7c53f293ba2f
Dirk Rathje <post@dirk-rathje.de>
2024-05-06 06:04:53 +0200

    fix: pwa support

--- packages/@befide/bf2035-report/pwa-assets.config.ts
+++ packages/@befide/bf2035-report/pwa-assets.config.ts
@@ -1,5 +1,5 @@
 import {
-  createAppleSplashScreens,
+  
   defineConfig,
   minimal2023Preset,
 } from "@vite-pwa/assets-generator/config"
@@ -9,20 +9,7 @@ export default defineConfig({
     preset: "2023",
   },
   preset: {
-    ...minimal2023Preset,
-    appleSplashScreens: createAppleSplashScreens(
-      {
-        padding: 0.3,
-        resizeOptions: { fit: "contain", background: "white" },
-        darkResizeOptions: { fit: "contain", background: "black" },
-        linkMediaOptions: {
-          log: true,
-          addMediaScreen: true,
-          xhtml: true,
-        },
-      },
-      ['iPad Air 9.7"'],
-    ),
+    ...minimal2023Preset
   },
-  images: "public/icons/appicon.svg",
+  images: "public/appicon.svg",
 })
