55f804c45bd94da06a02ab6b383ca25c9266c04d
Dirk Rathje <post@dirk-rathje.de>
2024-06-02 10:36:39 +0200

    fix: linear navigation

--- packages/@befide/bf2035-report/src/astro/utils/getSections.ts
+++ packages/@befide/bf2035-report/src/astro/utils/getSections.ts
@@ -1,33 +1,44 @@
 import { getCollection, type CollectionEntry } from "astro:content"
+import { log } from "console"
 import { type HierarchyNode, stratify } from "d3-hierarchy"
+
 
 const sortByPath = (
   s1: CollectionEntry<"sections">,
   s2: CollectionEntry<"sections">,
-) => getPath(s1).localeCompare(getPath(s2))
+) => getSlug(s1).localeCompare(getSlug(s2))
 
-export const getPath = (section: CollectionEntry<"sections">) => {
+export function getSlug(section: CollectionEntry<"sections">) {
   if (!section?.slug) return
 
-  return (getPathParts(section).join("/") + "/").replaceAll("//", "/")
+  let slug = section.id.replace(/\.mdx?/, "/")
+
+  return slug
 }
 
-const normalizeSlug = (slug = "") => {
-  return (
-    "/" +
-    slug.replaceAll("--00--", "--").replaceAll("--", "/").replace("index", "")
-  )
+
+export function getPath(section: CollectionEntry<"sections">) {
+
+  return "/" + getSlug(section)
+
 }
 
-const getPathParts = (section: CollectionEntry<"sections">) => {
-  return normalizeSlug(section.slug)
-    .split("/")
-    .filter((p, i) => i === 1 || /^[0-9]*$/.test(p))
-}
+// const normalizeSlug = (slug = "") => {
+//   return (
+//     "/" +
+//     slug.replaceAll("--00--", "--").replaceAll("--", "/").replace("index", "")
+//   )
+// }
 
-export const getParentPath = (section: CollectionEntry<"sections">) => {
-  return getPathParts(section).slice(0, -1).join()
-}
+// const getIdParts = (section: CollectionEntry<"sections">) => {
+//   return normalizeSlug(section.id)
+//     .split("/")
+//     .filter((p, i) => i === 1 || /^[0-9]*$/.test(p))
+// }
+
+// export const getParentPath = (section: CollectionEntry<"sections">) => {
+//   return getPathParts(section).slice(0, -1).join()
+// }
 
 export async function getSections() {
   const sections = await getCollection("sections")
@@ -36,24 +47,32 @@ export async function getSections() {
 }
 
 export async function getPrevNext(currentPath = "") {
+  
   const sections = await getSections()
 
   const currentSectionIndex = sections.findIndex(
     (section) => getPath(section) === currentPath,
   )
 
+  console.log({currentPath, currentSectionIndex});
+  
+
   let prevSectionIndex = currentSectionIndex - 1
 
-  while (prevSectionIndex >= 0 && sections[prevSectionIndex].data.virtual)
+  while (prevSectionIndex >= 0 && sections[prevSectionIndex].data.excludeFromTour) {
     prevSectionIndex--
+  }
   if (prevSectionIndex === -1) prevSectionIndex = sections.length - 1
 
   let nextSectionIndex = currentSectionIndex + 1
   while (
-    nextSectionIndex < sections.length &&
-    sections[nextSectionIndex].data.virtual
-  )
+    nextSectionIndex < sections.length && sections[nextSectionIndex].data.excludeFromTour
+  ) {
     nextSectionIndex++
+  }
+
+  log({nextSectionIndex})
+
   if (nextSectionIndex === sections.length - 1) nextSectionIndex = 0
 
   return {
@@ -67,10 +86,13 @@ export async function getPrevNext(currentPath = "") {
 export type SectionTreeNode = HierarchyNode<CollectionEntry<"sections">>
 
 export async function getSectionsTreeRoot(): Promise<SectionTreeNode> {
+
   const sections = await getSections()
 
+  const filteredSections = sections.filter(s => !s.data.excludeFromToc)
+
   const root = stratify<CollectionEntry<"sections">>().path((d) => getPath(d))(
-    sections,
+    filteredSections,
   )
 
   return root
